6e88e2384aea14d7925aa9d457ce938b9cee863a
Venkata Arimilli <venkata.arimilli1@t-mobile.com>
2019-12-12 15:29:58 +0530

    creating sample project

---
+++ SampleGitFundamentals/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleGitFundamentals
+//
+//  Created by Venkata Rama Kishore on 12/12/19.
+//  Copyright © 2019 Venkata Rama Kishore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
